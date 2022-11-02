4c9bd06e9a45349ccb82bab9fe6846f14181630f
Thomas Strandenaes <thostr@gmail.com>
2022-11-02 21:42:52 +0100

    try use navigation

--- src/nvc-helper/App.js
+++ src/nvc-helper/App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View, Button, Alert} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const  MyApp = () => {
+  
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
@@ -23,22 +28,26 @@ const  MyApp = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Button
-  title="{item.feeling}"
-  onPress={() => Alert.alert('Left button pressed')}
-  color="#841584"
-  accessibilityLabel="{item.description}"
-/>
-          )}
-        />
-      )}
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+
+    //           <Text> {item.feeling} </Text>
+              <Button
+    title={item.feeling}
+    onPress={() => Alert.alert('Left button pressed')}
+    color="#841584"
+    accessibilityLabel={item.description}
+  />
+            )}
+          />
+        )}
+      </View>
+    </NavigationContainer>
   );
 };
 
